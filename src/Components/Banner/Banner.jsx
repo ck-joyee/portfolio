6aff9de5d45eaf2fa6c1e61d5797c0d57d9be754
Chakraborty-Joyee <joyeechakraborty2000@gmail.com>
2023-07-03 15:16:46 +0600

    contact done

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,7 +1,7 @@
 import { TypeAnimation } from "react-type-animation";
 import pic from "../../assets/port.2.jpg";
 import { FaGithub, FaFacebook, FaLinkedinIn } from "react-icons/fa";
-import {motion} from 'framer-motion';
+import { motion } from "framer-motion";
 import { fadeIn } from "../../Variants/Variants";
 
 const Banner = () => {
@@ -10,23 +10,28 @@ const Banner = () => {
       <div className="container mx-auto">
         <div className="flex flex-col gap-y-8 lg:flex-row lg:gap-x-12 items-center ms-10 me-10">
           <div className="flex-1 text-center lg:text-left">
-            <motion.h1 
-            variants={fadeIn('up', 0.3)}
-            initial='hidden'
-            whileInView={'show'}
-            viewport={{once: false, amount: 0.7}} 
-            className="text-[35px] font-bold leading-[1.5] lg:text-[70px] text-white">
+            <motion.h1
+              variants={fadeIn("up", 0.3)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="text-[35px] font-bold leading-[1.5] lg:text-[70px] text-white"
+            >
               Joyee <span>Chakraborty</span>
             </motion.h1>
-            <motion.div 
-            variants={fadeIn('up', 0.4)}
-            initial='hidden'
-            whileInView={'show'}
-            viewport={{once: false, amount: 0.7}} 
-            className="mb-6 text-[26px] lg:text-[40px] font-semibold uppercase leading-[1]">
+            <motion.div
+              variants={fadeIn("up", 0.4)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="mb-6 text-[26px] lg:text-[40px] font-semibold uppercase leading-[1]"
+            >
               <span className="mr-4 text-white">I am a</span>
               <TypeAnimation
-                sequence={["Front-End Developer!!!", "Programming Instructor!!!"]}
+                sequence={[
+                  "Front-End Developer!!!",
+                  "Programming Instructor!!!",
+                ]}
                 speed={0}
                 className="text-gradient"
                 wrapper="span"
@@ -35,55 +40,69 @@ const Banner = () => {
             </motion.div>
 
             <motion.p
-            variants={fadeIn('up', 0.5)}
-            initial='hidden'
-            whileInView={'show'}
-            viewport={{once: false, amount: 0.7}} 
-            className="mb-8 max-w-lg mx-auto lg:mx-0 text-white">
+              variants={fadeIn("up", 0.5)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="mb-8 max-w-lg mx-auto lg:mx-0 text-white"
+            >
               A Tech Enthusiast who is currently scrutinizing Web Development. I
               have worked as a Programming Instructor at Cybernetics Robo
               Academy. I am also pursuing my B.Sc in Computer Science &
               Engineering.
             </motion.p>
             <motion.div
-            variants={fadeIn('up', 0.6)}
-            initial='hidden'
-            whileInView={'show'}
-            viewport={{once: false, amount: 0.7}} 
-            className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0">
-              <button className="btn hover btn-lg text-black ">Resume</button>
-              <a href="#" className="btn-link">
+              variants={fadeIn("up", 0.6)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0"
+            >
+              <a
+                href="https://drive.google.com/file/d/1xVgAkZclUiE2FJrmC0tUj0_ugqhstp3h/view?usp=sharing"
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{ textDecoration: "none" }}
+              >
+                 <button className="btn hover  text-white ">Resume</button>
+              </a>
+
+              <a href="#" className="text-gradient">
                 Portfolio
               </a>
             </motion.div>
 
             <motion.div
-            variants={fadeIn('up', 0.7)}
-            initial='hidden'
-            whileInView={'show'}
-            viewport={{once: false, amount: 0.7}} 
-            className="flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0">
-              <a href="#">
+              variants={fadeIn("up", 0.7)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0"
+            >
+              <a href="https://github.com/ck-joyee">
                 <FaGithub />
               </a>
-              <a href="#">
+              <a href="https://www.linkedin.com/in/joyee-chakraborty-8b5b50229">
                 <FaLinkedinIn />
               </a>
-              <a href="#">
+              <a href="https://www.facebook.com/joyee.chakraborty.9822?mibextid=ZbWKwL">
                 <FaFacebook />
               </a>
             </motion.div>
           </div>
-
+         
           <motion.div
-           variants={fadeIn('down', 0.6)}
-           initial='hidden'
-           whileInView={'show'}
+            variants={fadeIn("down", 0.6)}
+            initial="hidden"
+            whileInView={"show"}
           >
-            <img 
-            width="350" 
-            height="100" 
-            className=" hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px] rounded-xl" src={pic} alt="" />
+            <img
+              width="350"
+              height="100"
+              className=" hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px] rounded-xl"
+              src={pic}
+              alt=""
+            />
           </motion.div>
         </div>
       </div>
--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,11 +1,114 @@
-
+import { motion } from "framer-motion";
+import emailjs from '@emailjs/browser';
+import phoneIcon from "../../icons/phone.png";
+import emailIcon from "../../icons/email.png";
+import addressIcon from "../../icons/address.png";
+import { useRef } from "react";
 
 const Contact = () => {
-    return (
-        <div className="section" id="contact">
-           Contact 
+    const form = useRef();
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_qr58naj', 'template_auvztu1', form.current, 'BZcEPesCZmLP0-xsC')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+      };
+  return (
+    <div className="section font-mono" id="contact">
+      <div className="container mx-auto p-8">
+        <div className="flex flex-col items-center">
+          <motion.div
+            className="mb-4 mt-3 contact-sec"
+            initial={{ scale: 0, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            transition={{ duration: 0.5 }}
+          >
+            <h4 className="text-4xl mb-4 text-gradient">Contact Me!!!</h4>
+            <form
+              ref={form}
+              onSubmit={sendEmail}
+            >
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                <div>
+                  <label htmlFor="name" className="block mb-2 text-gradient">
+                    Full Name
+                  </label>
+                  <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    className="w-full text-white mb-2 px-4 py-2"
+                    required
+                  />
+                </div>
+                <div>
+                  <label htmlFor="email" className="block mb-2 text-gradient">
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    className="w-full text-white mb-2 px-4 py-2"
+                    required
+                  />
+                </div>
+              </div>
+              <label htmlFor="message" className="block mb-2 text-gradient">
+                Message
+              </label>
+              <textarea
+                id="message"
+                name="message"
+                rows={6}
+                className="w-full text-white mb-2 px-4 py-2"
+                required
+              ></textarea>
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+                <div className="text-center my-10 text-gradient ">
+                  <img
+                    src={addressIcon}
+                    alt="Address"
+                    className="w-20 h-20 mx-auto mb-2"
+                  />
+                  <p>Address</p>
+                  <p>Shibgonj, Sylhet, Bangladesh</p>
+                </div>
+                <div className="text-center my-10 text-gradient">
+                  <img
+                    src={phoneIcon}
+                    alt="Phone"
+                    className="w-20 h-20 mx-auto mb-2"
+                  />
+                  <p>Phone</p>
+                  <p>01624068657</p>
+                </div>
+                <div className="text-center my-10 text-gradient">
+                  <img
+                    src={emailIcon}
+                    alt="Email"
+                    className="w-20 h-20 mx-auto mb-2"
+                  />
+                  <p>Email</p>
+                  <p>joyeeck04@gmail.com</p>
+                </div>
+              </div>
+              <button
+                type="submit"
+                className="btn mt-10 text-white py-2 px-4 rounded-lg w-3/4 mx-20"
+              >
+                Submit Form
+              </button>
+            </form>
+          </motion.div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
